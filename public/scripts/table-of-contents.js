eebdf159bba007a5ff35d8da09d95c045529261e
Samuel He <samuelhe52@outlook.com>
2025-11-16 01:09:49 +0800

    Revert "Fix TOC animation glitches"
    This reverts commit 807d310113f3ca2eb3e2ea0e7b5845fc21ca2287.

--- public/scripts/table-of-contents.js
+++ public/scripts/table-of-contents.js
@@ -59,12 +59,6 @@ function generateTOC() {
   tocList.appendChild(fragment);
   console.log('TOC: Items added:', tocList.children.length);
 
-  // Show TOC with fade-in
-  const toc = document.querySelector('.toc');
-  if (toc) {
-    toc.classList.add('loaded');
-  }
-
   // Highlight active section on scroll
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
