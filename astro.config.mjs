6e652a50c70538f6dda7e57fac3737cfe5307b75
Samuel He <samuelhe52@outlook.com>
2025-11-30 02:53:52 +0800

    Support Katex Rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://blog.konakona52.com',
   trailingSlash: 'always',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   integrations: [
     mdx(),
     sitemap({
